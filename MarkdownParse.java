47f37ce0641713407f1f8b33b39eac1bced0ae81
choi8616 <97643728+choi8616@users.noreply.github.com>
2022-01-19 17:56:14 -0800

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,6 +12,7 @@ public class MarkdownParse {
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
+            //we need to find a closing bracket following immediately by an opening paranthesis
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
@@ -22,8 +23,14 @@ public class MarkdownParse {
         return toReturn;
     }
     public static void main(String[] args) throws IOException {
+        if(args.length == 0) {
+            System.out.println("no argument");
+            return;
+        }
 		Path fileName = Path.of(args[0]);
 	    String contents = Files.readString(fileName);
+        //split contents into string array
+        //for loop: for each content check for "](" and "https://"
         ArrayList<String> links = getLinks(contents);
         System.out.println(links);
     }
