f28983f612f97bdb8ec3d8a152d10992c45350fa
choi8616 <97643728+choi8616@users.noreply.github.com>
2022-03-11 11:43:17 -0800

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -6,21 +6,6 @@ import java.util.ArrayList;
 
 public class MarkdownParse {
 
-    // Loop with a stack until finding the corresponding closeParen
-    static int findCloseParen(String markdown, int openParen) {
-        int closeParen = openParen + 1;
-        int openParenCount = 1;
-        while (openParenCount > 0) {
-            if (markdown.charAt(closeParen) == '(') {
-                // openParenCount++;
-            } else if (markdown.charAt(closeParen) == ')') {
-                openParenCount--;
-            }
-            closeParen++;
-        }
-        return closeParen - 1;
-
-    }
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then take up to
@@ -28,12 +13,11 @@ public class MarkdownParse {
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
-            // System.out.format("%d\t%d\t%s\n", currentIndex, nextOpenBracket, toReturn);
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
 
             // The close paren we need may not be the next one in the file
-            int closeParen = findCloseParen(markdown, openParen);
+            int closeParen = markdown.indexOf(")", openParen);
             
             if(nextOpenBracket == -1 || nextCloseBracket == -1
                   || closeParen == -1 || openParen == -1) {
